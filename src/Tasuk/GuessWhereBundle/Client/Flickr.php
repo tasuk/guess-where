8d0dcec4d297aba4443028ab5d2afc5a8f24affa
Vit Brunner <vit.brunner@gmail.com>
2013-02-03 16:37:14 +0000

    flickr api top photo getting

--- src/Tasuk/GuessWhereBundle/Client/Flickr.php
+++ src/Tasuk/GuessWhereBundle/Client/Flickr.php
@@ -4,10 +4,15 @@ namespace Tasuk\GuessWhereBundle\Client;
 
 use Guzzle\Http\ClientInterface;
 
+/**
+ * Communication with Flickr API
+ */
 class Flickr extends Client
 {
     const PLACE_TYPE_REGION = 8;
     const PLACE_TYPE_COUNTRY = 12;
+    const CONTENT_PHOTOS_ONLY = 1;
+    const SORT_BY_INTERESTINGNESS = "interestingness-desc";
 
     /**
      * Get top photo places
@@ -22,8 +27,34 @@ class Flickr extends Client
         $request->getQuery()
             ->set('api_key', $this->apiKey)
             ->set('method', 'flickr.places.getTopPlacesList')
-            ->set('place_type_id', $type)
-        ;
+            ->set('place_type_id', $type);
+
+        return $request->send();
+    }
+
+    /**
+     * Get top photos for place
+     *
+     * @param int    $woeid  Location WOEID
+     * @param string $maxAge Max photo age, eg. "5 years"
+     * @param int    $limit  Number of items to get
+     *
+     * @return Guzzle\Http\Message\Response
+     */
+    public function getTopPhotos($woeid, $maxAge, $limit)
+    {
+        $today = strtotime(date("Y-m-d"));
+        $minUploadDate = strtotime('-' . $maxAge, $today);
+
+        $request = $this->guzzleClient->get();
+        $request->getQuery()
+            ->set('api_key', $this->apiKey)
+            ->set('method', 'flickr.photos.search')
+            ->set('sort', self::SORT_BY_INTERESTINGNESS)
+            ->set('content_type', self::CONTENT_PHOTOS_ONLY)
+            ->set('woe_id', $woeid)
+            ->set('min_upload_date', $minUploadDate)
+            ->set('per_page', $limit);
 
         return $request->send();
     }
--- src/Tasuk/GuessWhereBundle/Tests/Client/FlickrTest.php
+++ src/Tasuk/GuessWhereBundle/Tests/Client/FlickrTest.php
@@ -13,13 +13,24 @@ class FlickrTest extends ContainerAwareTestCase
     public function testGetTopPlaces()
     {
         $flickr = $this->getContainer()->get('guesswhere.flickr_client');
-        $simpleXml = $flickr->getTopPlaces();
+        $response = $flickr->getTopPlaces();
 
         $places = array();
-        foreach ($simpleXml->xml()->places->place as $place) {
+        foreach ($response->xml()->places->place as $place) {
             $places[] = (string) $place;
         }
 
-        $this->assertContains("Ukraine", $places);
+        $this->assertContains('Ukraine', $places);
+    }
+
+    public function testGetTopPhotos()
+    {
+        $flickr = $this->getContainer()->get('guesswhere.flickr_client');
+        $response = $flickr->getTopPhotos(23424976, '6 months', 10);
+
+        // be happy with any numeric photo id
+        $this->assertTrue(is_numeric(
+            (string) $response->xml()->photos->photo['id']
+        ));
     }
 }
