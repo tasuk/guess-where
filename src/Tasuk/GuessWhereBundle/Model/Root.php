929b46e2d136a1ab45c9b10dc86c615c0945b83b
Vit Brunner <vit.brunner@gmail.com>
2013-02-24 13:24:57 +0000

    fix coding standard

--- src/Tasuk/GuessWhereBundle/Model/Root.php
+++ src/Tasuk/GuessWhereBundle/Model/Root.php
@@ -5,6 +5,8 @@ namespace Tasuk\GuessWhereBundle\Model;
 use FSC\HateoasBundle\Annotation as Hateoas;
 
 /**
+ * Base API root node
+ *
  * @Hateoas\Relation("games", href = @Hateoas\Route("post_games"))
  */
 class Root
