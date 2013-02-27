d370639e43e2805796cb62ac32ac1c87035d5377
Vit Brunner <vit.brunner@gmail.com>
2013-02-27 22:43:34 +0000

    add self link to root

--- src/Tasuk/GuessWhereBundle/Model/Root.php
+++ src/Tasuk/GuessWhereBundle/Model/Root.php
@@ -7,6 +7,7 @@ use FSC\HateoasBundle\Annotation as Hateoas;
 /**
  * Base API root node
  *
+ * @Hateoas\Relation("self", href = @Hateoas\Route("get"))
  * @Hateoas\Relation("games", href = @Hateoas\Route("post_games"))
  */
 class Root
